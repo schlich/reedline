bcdffcc7694aabec3fa1e110bff87d1cd6a4bd80
schlich <ty.schlich@gmail.com>
2026-02-16 19:13:33 +0000

    hx: improve command handling in edit loop

--- src/edit_mode/hx.rs
+++ src/edit_mode/hx.rs
@@ -1,7 +1,7 @@
-use modalkit::prelude::{MoveDir1D, MoveType};
 use modalkit::keybindings::{
     BindingMachine, EmptyKeyState, InputBindings, InputKey, ModalMachine, Mode, ModeKeys, EdgeEvent, EdgeRepeat
 };
+use modalkit::prelude::{MoveDir1D, MoveType};
 
 const ESC:char = '\u{1B}';
 
@@ -15,9 +15,18 @@ enum HelixMode {
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 enum HelixAction {
     Type(char),
-    Move(MoveType),
+    MoveCharLeft,
     #[default]
     NoOp,
+}
+
+impl HelixAction {
+    fn to_move_type(&self) -> Option<MoveType> {
+        match self {
+            HelixAction::MoveCharLeft => Some(MoveType::Column(MoveDir1D::Previous, false)),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Default)]
@@ -54,7 +63,7 @@ impl InputBindings<char, HelixStep> for HelixBindings {
         machine.add_mapping(
             HelixMode::Normal,
             &[(EdgeRepeat::Once, EdgeEvent::Key('h'))],
-            &(Some(HelixAction::Move(MoveType::Column(MoveDir1D::Previous, false))), None),
+            &(Some(HelixAction::MoveCharLeft), None),
         );
     }
 }
@@ -68,6 +77,28 @@ pub type HelixMachine = ModalMachine<char, HelixStep>;
 mod test {
 
     use super::*;
+    use modalkit::editing::application::EmptyInfo;
+    use modalkit::editing::buffer::EditBuffer;
+    use modalkit::editing::cursor::Cursor;
+    use modalkit::editing::store::Store;
+    use modalkit::prelude::{Count, EditTarget, ViewportContext};
+    use modalkit::actions::{EditAction, EditorActions};
+    use modalkit::env::vim::VimState;
+
+    fn mkfivestr(
+        s: &str,
+    ) -> (EditBuffer<EmptyInfo>, modalkit::editing::buffer::CursorGroupId, ViewportContext<Cursor>, VimState, Store<EmptyInfo>)
+    {
+        let mut buf = EditBuffer::new("".to_string());
+        let gid = buf.create_group();
+        let vwctx = ViewportContext::default();
+        let vctx = VimState::default();
+        let store = Store::default();
+
+        buf.set_text(s);
+
+        (buf, gid, vwctx, vctx, store)
+    }
 
     #[test]
     fn test_insert_mode_is_default() {
@@ -90,11 +121,19 @@ mod test {
         let _ = machine.pop();
 
         machine.input_key('h');
-        let action = machine.pop();
-        assert_eq!(
-            action,
-            Some((HelixAction::Move(MoveType::Column(MoveDir1D::Previous, false)), EmptyKeyState::default()))
-        )
+        let (action, _) = machine.pop().unwrap();
+        assert_eq!(action, HelixAction::MoveCharLeft);
+
+        let mv = action.to_move_type().expect("MoveCharLeft should map to a MoveType");
+
+        let (mut ebuf, gid, vwctx, vctx, mut store) = mkfivestr("hello\n");
+        ebuf.set_leader(gid, Cursor::new(0, 3));
+        assert_eq!(ebuf.get_leader(gid), Cursor::new(0, 3));
+
+        let target = EditTarget::Motion(mv, Count::Exact(1));
+        let ctx = &(gid, &vwctx, &modalkit::editing::context::EditContext::default());
+        ebuf.edit(&EditAction::Motion, &target, ctx, &mut store).unwrap();
+        assert_eq!(ebuf.get_leader(gid), Cursor::new(0, 2));
     }
 
 }
